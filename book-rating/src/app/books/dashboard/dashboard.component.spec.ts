479274831753589929433a01cfa7dd1159886464
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-04-28 14:39:12 +0200

    real unit-test (BookComponentStub)

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,7 +1,19 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookComponent } from '../book/book.component';
+import { Book } from '../shared/book';
 
 import { DashboardComponent } from './dashboard.component';
+
+@Component({
+  selector: 'br-book',
+  template: 'HEy!!',
+})
+export class BookComponentStub {
+  @Input()
+  book?: Book;
+}
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -11,7 +23,8 @@ describe('DashboardComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         DashboardComponent,
-        BookComponent // Integration-Test
+        // BookComponent // Integration-Test
+        BookComponentStub
       ]
     })
     .compileComponents();
