c793d7a54183ebfeca9228c8207d3eb4a64dd98e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-05-02 13:02:41 +0200

    forms

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -14,4 +14,13 @@ export class CreateBookComponent {
     title: new FormControl('', Validators.required),
     description: new FormControl('')
   });
+
+  isInvalid(path: string): boolean {
+    const control = this.bookForm.get(path);
+    return !!control && control.touched && control.invalid;
+  }
+
+  // optionales Hands On:
+  // hasError(path: string, code: string): boolean
+  // --> <div *ngIf="hasError('isbn', 'minlength')">Bitte mindestens 3 Zeichen eingeben</div>
 }
