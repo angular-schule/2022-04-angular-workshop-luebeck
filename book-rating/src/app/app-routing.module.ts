0cfb0c6c050411afea2365dd68001eaf265d98f9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-05-03 11:42:01 +0200

    routerLink

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'books' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
